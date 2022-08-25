35dc0e1a2bcdfd2ed33effe206cf2cbb2e34381c
shikharsharma-zeotap <shikhar.sharma@zeotap.com>
2022-08-26 02:58:09 +0530

    styling datastore table

---
+++ src/utils.ts
@@ -0,0 +1,19 @@
+export const formatAddress = (addr: string): string => addr?.slice(0, 4) + '....' + addr?.slice(addr.length - 4, addr.length)
+
+export const formatBigNumber = (labelValue: string) => {
+  // Nine Zeroes for Billions
+  const val = Math.round(+labelValue/1e+18).toString()
+  return Math.abs(Number(val)) >= 1.0e+9
+
+    ? Math.abs(Number(val)) / 1.0e+9 + "B"
+    // Six Zeroes for Millions 
+    : Math.abs(Number(val)) >= 1.0e+6
+
+      ? Math.abs(Number(val)) / 1.0e+6 + "M"
+      // Three Zeroes for Thousands
+      : Math.abs(Number(val)) >= 1.0e+3
+
+        ? Math.abs(Number(val)) / 1.0e+3 + "K"
+
+        : Math.abs(Number(val));
+}
